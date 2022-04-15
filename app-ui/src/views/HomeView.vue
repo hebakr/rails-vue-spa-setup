c02ced4adad2fcbac6553dc6288373d7386b16dd
Abubakr Mohamed <hebakr@gmail.com>
2022-04-15 04:55:01 +0200

    basic setup

---
+++ app-ui/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Rails/Vue basic setup!</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
